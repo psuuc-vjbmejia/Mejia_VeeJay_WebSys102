3d35c538124481ad32037b7e2988cb55058068f8
VeeJay <132413717+psuuc-vjbmejia@users.noreply.github.com>
2025-03-07 21:33:18 +0800

    Delete Activity 6 directory

--- Activity 6/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-use App\Http\Controllers\FormController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/form', [FormController::class, 'showForm']) -> name('Form');
-Route::post('/form', [FormController::class, 'handleForm']);
-
